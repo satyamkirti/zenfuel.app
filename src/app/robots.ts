35e3b7628504dd2421385dd793317e9f7b9aeadb
Satyam Kirti <satyam.kirti@gmail.com>
2026-07-07 15:27:23 +0530

    Fix robots.txt - allow Google to crawl all pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,12 @@ export const dynamic = 'force-static';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://zenfuel.app/sitemap.xml',
   };
 }
